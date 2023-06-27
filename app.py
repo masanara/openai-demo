8a4cbe9cf7d853f420630229140ed95d5988cc14
masanara <masanara@gmail.com>
2023-06-27 01:47:01 +0000

    add ansible playbook for PAN

--- app.py
+++ app.py
@@ -19,19 +19,38 @@ def index():
 @app.route('/answer', methods=['post'])
 def answer():
     prompt = request.form.get('prompt')
-    response = openai.Completion.create(
-      model="text-davinci-003",
-      #model="gpt-3.5-turbo",
-      prompt=prompt,
-      temperature=0,
-      max_tokens=3000,
-      top_p=1,
-      frequency_penalty=0.0,
-      presence_penalty=0.0,
-      #stop=["\n"]
-    )
-    #print(response.choices[0].text) 
-    res=response.choices[0].text
-    return render_template('answer.html',res=res)
+    try:
+        response = openai.Completion.create(
+          model="text-davinci-003",
+          #model="gpt-3.5-turbo",
+          prompt=prompt,
+          temperature=0,
+          max_tokens=3000,
+          top_p=1,
+          frequency_penalty=0.0,
+          presence_penalty=0.0,
+          #stop=["\n"]
+        )
+        #print(response.choices[0].text) 
+        res=response.choices[0].text
+        return render_template('answer.html',res=res)
+    except openai.error.APIError as e:
+      #Handle API error here, e.g. retry or log
+      print(f"OpenAI API returned an API Error: {e}")
+      res=f"OpenAI API returned an API Error: {e}"
+      return render_template('error.html',res=res)
+      pass
+    except openai.error.APIConnectionError as e:
+      #Handle connection error here
+      print(f"Failed to connect to OpenAI API: {e}")
+      res=f"Failed to connect to OpenAI API: {e}"
+      return render_template('error.html',res=res)
+      pass
+    except openai.error.RateLimitError as e:
+      #Handle rate limit error (we recommend using exponential backoff)
+      print(f"OpenAI API request exceeded rate limit: {e}")
+      res=f"OpenAI API request exceeded rate limit: {e}"
+      return render_template('error.html',res=res)
+      pass
 
 app.run(host='0.0.0.0', port=8000, debug=True)
